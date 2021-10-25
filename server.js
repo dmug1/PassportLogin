433233c61589ad9413bb35dd7926fe267cb8e477
Diego Mugnae <diegomugnae@gmail.com>
2021-10-25 16:51:57 +1000

    add bcrypt
    BCRYPT will be used to hash the passcodes

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express =  require('express')
 const app = express()
 
 app.set('view-engine','ejs')
+app.use(express.urlencoded({ extended:false}))  //come back to study this
+                                                // not really clear yet
 
+const users = []
 
  //set routes
  app.get('/',(req,res) =>{
@@ -14,13 +17,18 @@ app.get('/login',(req,res) =>{
     res.render('login.ejs')
 })
 
+app.post('/login', (req,res) =>{
+    
+})
+
+
 //set login register
 app.get('/register',(req,res) =>{
     res.render('register.ejs')
 })
 
 app.post('/register', (req,res) =>{
-    
+    req.body.email
 })
 
 
