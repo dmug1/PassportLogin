dfa8b19e169c6bbb19a1ade6c3bb531c9b76351e
Diego Mugnae <diegomugnae@gmail.com>
2021-10-25 17:13:36 +1000

    setting register to persist data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express =  require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 app.set('view-engine','ejs')
 app.use(express.urlencoded({ extended:false}))  //come back to study this
@@ -27,8 +28,25 @@ app.get('/register',(req,res) =>{
     res.render('register.ejs')
 })
 
-app.post('/register', (req,res) =>{
-    req.body.email
+app.post('/register', async (req, res) => {
+    try {
+
+        const hashedPassword = await bcrypt.hash(req.body.hashedPassword, 10) // async        
+        
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch{
+        console.log('fail');
+        res.redirect('/register')
+        
+    }
+    console.log(users)
+
 })
 
 
